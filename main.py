814ebf885973936c1f3e49697ad0e5a98f9f6b9a
TIMcre <92308299+TIMcre@users.noreply.github.com>
2022-01-25 20:07:07 +0100

    Update main.py
    removed unused imports

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.common.exceptions import TimeoutException
 import time
 # inital setup
 driver = webdriver.Firefox(executable_path="C:\\Users\\Timon\\geckodriver.exe")
