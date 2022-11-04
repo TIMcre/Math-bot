45e635b6988533cd230a0de45d5d2bb8e27a4e43
TIMcre <nhom@web.de>
2022-11-04 18:00:00 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
 from tqdm.auto import tqdm
 import pandas as pd
-times = int(input("how often "))
 
-# inital setup
-driver = webdriver.Firefox(executable_path="geckodriver.exe")
-driver.get("https://www.realmath.de/Neues/Klasse8/binome/binomevar03.php")
-# checkbox for alowing input
-abox = driver.find_element(By.XPATH,"/html/body/form/div[2]/div/div/div/div[3]/center/table/tbody/tr/td[1]/input")
-abox.click()
-# input field for answer
-ibox = driver.find_element(By.CSS_SELECTOR,"#idxt1")
-# box to check the answer
-cbox = driver.find_element(By.CSS_SELECTOR,".pruefeButton")
-# box to make a new question
-gbox = driver.find_element(By.CSS_SELECTOR,".neuButton")
-# question field
-qbox = driver.find_element(By.CSS_SELECTOR,"#idz1")
 
 def get_answer(question):
     df = pd.read_csv("data.csv", encoding='utf8')
@@ -39,5 +25,27 @@ def task(driver, gbox, qbox, ibox, cbox):
     ibox.send_keys(a)
     cbox.click()
 
-for _ in tqdm(range(times)):
-    task(driver, gbox, qbox, ibox, cbox)
+print(f"[Debug] loading browser")
+# inital setup
+driver = webdriver.Firefox(service=Service("geckodriver.exe"))
+driver.get("https://www.realmath.de/Neues/Klasse8/binome/binomevar03.php")
+# checkbox for alowing input
+abox = driver.find_element(By.XPATH,"/html/body/form/div[2]/div/div/div/div[3]/center/table/tbody/tr/td[1]/input")
+abox.click()
+# input field for answer
+ibox = driver.find_element(By.CSS_SELECTOR,"#idxt1")
+# box to check the answer
+cbox = driver.find_element(By.CSS_SELECTOR,".pruefeButton")
+# box to make a new question
+gbox = driver.find_element(By.CSS_SELECTOR,".neuButton")
+# question field
+qbox = driver.find_element(By.CSS_SELECTOR,"#idz1")
+
+print(f"[Debug] finished loading browser")
+times = int(input(f"[Input] iterations: "))
+try:
+    for _ in tqdm(range(times)):
+        task(driver, gbox, qbox, ibox, cbox)
+except:
+    print(f"[Error] invalid input")
+print(f"[Debug] finished")
