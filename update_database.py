5fdd476906cbf9f88e26d31323f0cfd2a8c72765
TIMcre <nhom@web.de>
2022-11-04 17:57:45 +0100

    update update_database.py

--- update_database.py
+++ update_database.py
@@ -1,16 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
 from tqdm.auto import tqdm
 import pandas as pd
-# inital setup
-driver = webdriver.Firefox(executable_path="geckodriver.exe")
-driver.get("https://www.realmath.de/Neues/Klasse8/binome/binomevar03.php")
-df = pd.read_csv("data.csv", encoding="utf8")
 
-# box to show the answer
-sbox = driver.find_element(By.CSS_SELECTOR,".hilfButton")
-# box to make a new question
-gbox = driver.find_element(By.CSS_SELECTOR,".neuButton")
 
 def get_data(driver, sbox, gbox):
     # generate new question and answer
@@ -28,12 +21,28 @@ def get_data(driver, sbox, gbox):
 
 def add_data(data, df):
     df.loc[len(df.index)] = data    
-
-for _ in tqdm(range(500)):
-    add_data(get_data(driver, sbox, gbox), df)
     
+print(f"[Debug] loading browser")  
+# inital setup
+driver = webdriver.Firefox(service=Service("geckodriver.exe"))
+driver.get("https://www.realmath.de/Neues/Klasse8/binome/binomevar03.php")
+df = pd.read_csv("data.csv", encoding="utf8")
+
+# box to show the answer
+sbox = driver.find_element(By.CSS_SELECTOR,".hilfButton")
+# box to make a new question
+gbox = driver.find_element(By.CSS_SELECTOR,".neuButton")
+
+print(f"[Debug] finished loading browser")
+print(f"[Debug] gathering data")
+try:
+    for _ in tqdm(range(10)):
+        add_data(get_data(driver, sbox, gbox), df)    
+except:
+    print(f"[Error] invalid input")
 
 driver.quit
-
+print(f"[Debug] processing new data")
 df = df.drop_duplicates()
 df.to_csv("data.csv", encoding='utf8', index= False)
+print(f"[Debug] finished")
