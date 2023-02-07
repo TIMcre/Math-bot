2b2ac5a24d33826037378fc973fe373b385b9222
TIMcre <92308299+TIMcre@users.noreply.github.com>
2023-02-07 17:38:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 from tqdm.auto import tqdm
 import pandas as pd
+import re
 
+times = 50
+
+# inital setup
+options = Options()
+options.binary_location= "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
+driver = webdriver.Firefox(executable_path="C:\\Users\\Timon\\Downloads\\Math-bot-v.1.1\\geckodriver", options=options)
+
+driver.get("https://www.realmath.de/Neues/Klasse8/binome/binomevar03.php")
+# checkbox for alowing input
+abox = driver.find_element(By.XPATH,"/html/body/form/div[2]/div/div/div/div[3]/center/table/tbody/tr/td[1]/input")
+abox.click()
+# input field for answer
+ibox = driver.find_element(By.CSS_SELECTOR,"#idxt1")
+# box to check the answer
+cbox = driver.find_element(By.CSS_SELECTOR,".pruefeButton")
+# box to make a new question
+gbox = driver.find_element(By.CSS_SELECTOR,".neuButton")
+# question field
+qbox = driver.find_element(By.CSS_SELECTOR,"#idz1")
 
 def get_answer(question):
-    df = pd.read_csv("data.csv", encoding='utf8')
+    df = pd.read_csv("C:\\Users\\Timon\\Downloads\\Math-bot-v.1.1\\data.csv", encoding='utf8')
     ans = df[df["question"] == question]
     ans = ans.squeeze()
     ans = ans["answer"]
@@ -21,31 +41,18 @@ def get_question(driver, gbox, qbox):
 
 def task(driver, gbox, qbox, ibox, cbox):
     q = get_question(driver, gbox, qbox)
+
+    if re.match(".*\+.*\+.*", q):
+        print(f" | {q} | 1. Binomische Formel | 1. Type |")
+    if re.match(".*-.*\+.*", q):
+        print(f" | {q} | 2. Binomische Formel | 1. Type |")
+    if re.match("^x.*-[^x]* ",q):
+        print(f" | {q} | 3. Binomische Formel | 1. Type |")
+
     a = get_answer(q)
     ibox.send_keys(a)
     cbox.click()
 
-print(f"[Debug] loading browser")
-# inital setup
-driver = webdriver.Firefox(service=Service("geckodriver.exe"))
-driver.get("https://www.realmath.de/Neues/Klasse8/binome/binomevar03.php")
-# checkbox for alowing input
-abox = driver.find_element(By.XPATH,"/html/body/form/div[2]/div/div/div/div[3]/center/table/tbody/tr/td[1]/input")
-abox.click()
-# input field for answer
-ibox = driver.find_element(By.CSS_SELECTOR,"#idxt1")
-# box to check the answer
-cbox = driver.find_element(By.CSS_SELECTOR,".pruefeButton")
-# box to make a new question
-gbox = driver.find_element(By.CSS_SELECTOR,".neuButton")
-# question field
-qbox = driver.find_element(By.CSS_SELECTOR,"#idz1")
 
-print(f"[Debug] finished loading browser")
-times = int(input(f"[Input] iterations: "))
-try:
-    for _ in tqdm(range(times)):
-        task(driver, gbox, qbox, ibox, cbox)
-except:
-    print(f"[Error] invalid input")
-print(f"[Debug] finished")
+for _ in tqdm(range(times)):
+    task(driver, gbox, qbox, ibox, cbox)
